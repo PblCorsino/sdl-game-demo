d6a84ec358dafea6335710b777038694c409ae06
Pablo Corsino <pbl.corsino@gmail.com>
2017-06-02 00:01:52 +0200

    Added a contant frame rate in the main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
 #include<iostream>
 #include"include/Game.hpp"
 
+// Frames per second
+const int FPS = 60;
+// Miliseconds per frame
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main() {
+  Uint32 frameStart, frameTime;
+
   std::cout << "Game init attempt..." << std::endl;
 
   if (TheGame::Instance()->init("SDL game demo", 100, 100, 640, 480, false)) {
     std::cout << "Game init success" << std::endl;
     while (TheGame::Instance()->running()) {
+      frameStart = SDL_GetTicks();
+
       TheGame::Instance()->handleEvents();
       TheGame::Instance()->update();
       TheGame::Instance()->render();
 
-      SDL_Delay(10);
+      frameTime = SDL_GetTicks() - frameStart;
+
+      if (frameTime < DELAY_TIME) {
+        SDL_Delay((int)(DELAY_TIME - frameTime));
+      }
     }
   }
   else {
